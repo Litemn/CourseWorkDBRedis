90f06bc9d5e93663dac3b8403a34987188697d13
litemn <litlemn@gmail.com>
2015-06-01 16:07:13 +0300

    fix

--- src/main/java/DB.kt
+++ src/main/java/DB.kt
@@ -22,9 +22,11 @@ fun main(args: Array<String>) {
 
                 val it: Iterator<String> = args.iterator()
                 var ID = jedis.get("MAXID")
-                it.next()
+
                 var map:MutableMap<String,String> = HashMap<String, String>()
-                if(it.next().toLowerCase() == "id"){
+                if(it.hasNext()&&it.next().toLowerCase() == "id"){
+                    it.next()
+
                    ID = it.next()
 
                     if(ID !is String) {
@@ -52,7 +54,7 @@ fun main(args: Array<String>) {
                             val pattern: Regex = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$".toRegex()
                             value = it.next()
                             if (value.matches(pattern)) {
-                                map.put("daterelease", it.next())
+                                map.put("daterelease", value)
                             } else {
                                 println("Wrong date release")
                             }
@@ -61,10 +63,10 @@ fun main(args: Array<String>) {
                             map.put("mileage", it.next())
                         }
                         "pts" -> {
-                            val pattern : Regex = "^[0-9]{2}[а-яА-Я]{2}[0-9]+$".toRegex()
+                            val pattern : Regex = "^[0-9]{2}_[а-яА-Я]{2}_[0-9]+$".toRegex()
                             value = it.next()
                             if(value.matches(pattern)) {
-                                map.put("pts", value)
+                                map.put("pts",  value.replace("_"," "))
                             }else{
                                 println("Wrong pts")
                             }
@@ -84,6 +86,7 @@ fun main(args: Array<String>) {
                         }
                         else -> {
                             println("param "+p+" wrong")
+                            if(it.hasNext())
                             it.next()
                         }
 
@@ -95,13 +98,13 @@ fun main(args: Array<String>) {
 
                 map.put("id_car",ID)
                 jedis.hmset("car:id:" +ID, map)
-                println("changes blabla id is "+ ID)
+                println("changes item with id "+ ID)
 
             }
 
             "del"->{
 
-                if(args.size()==2) {
+                if(args.size()>=2) {
                     println("R you sure?(yes/no)")
                     val temp = readLine().toString()
                     if (temp.startsWith("y")) {
@@ -117,21 +120,18 @@ fun main(args: Array<String>) {
 
             }
 
-            "x" -> {
-                if(args.size()>=4) {
-                    val map = HashMap<String, String>()
-                    val it : Iterator<String> = args.copyOfRange(2,args.size()).iterator()
-                    while(it.hasNext()) {
-                        val temp = it.next()
-                        if(temp in param){
-                          //  TODO edit params
-                        } else {
-                            error("Wrong")
+            "get"-> {
+                if(args.size()>1) {
+                    if(args[1].matches("[0-9]+".toRegex())) {
+
+                        var map: MutableMap<String, String> = HashMap<String, String>()
+                        map=jedis.hgetAll("car:id:"+args[1])
+                        for(item in map){
+                            println(item.getKey()+" - "+item.getValue())
                         }
                     }
 
                 }
-
             }
 
             else -> print("error")
